2f55fbc6e63b3ba50d22ea66a5411cd840b06374
Andres Icedo <andresicedo24@gmail.com>
2021-04-09 11:44:17 -0500

    updated 0409

--- algo0409.js
+++ algo0409.js
@@ -22,15 +22,21 @@ racecar => true; truck => false
 // function isPalindrome(string) {
 //     let spiltString = string.split("");
 //     let reversedString = string.split("").reverse();
-//     for (let i = 0; i < spiltString.length; i++) {
-//         const letter = spiltString[i];
-//         if(letter === reversedString[i]) {
-//             continue;
-//         } else {
-//             return false
-//         }
+//     let join = reversedString.join("")
+//     // for (let i = 0; i < spiltString.length; i++) {
+//     //     const letter = spiltString[i];
+//     //     if(letter === reversedString[i]) {
+//     //         continue;
+//     //     } else {
+//     //         return false
+//     //     }
+//     // }
+//     // return true
+//     if (string === join) {
+//         return true
+//     } else {
+//         return false
 //     }
-//     return true
 // }
 
 // console.log(isPalindrome('racecar'));
@@ -54,24 +60,45 @@ function median(array) {
     return sorted[middleIndex];
 }
 
-function mode(array) {
-    let store = {}
-    for (let i = 0; i < array.length; i++) {
-        const number = array[i];
-        if(!store[number]){
-            store[number] = 1
-        } else {
-            store[number]++
-        }
-    }
-    let keys = Object.keys(store)
-    let mostCommon = store[keys[0]]
-    for (const key in store) {
-        if(key.value > mostCommon) {
-            mostCommon = key
-        }
-    }
-    return mostCommon
-}
+// function mode(array) {
+//     let store = {}
+//     for (let i = 0; i < array.length; i++) {
+//         const number = array[i];
+//         if(!store[number]){
+//             store[number] = 1
+//         } else {
+//             store[number]++
+//         }
+//     }
+//     let keys = Object.keys(store)
+//     let mostCommon = store[keys[0]]
+//     for (const key in store) {
+//         if(key.value > mostCommon) {
+//             mostCommon = key
+//         }
+//     }
+//     console.log(store)
+//     return mostCommon
+// }
 
-console.log(mode([4,3,1,2,2,7,9,14,2,27,400,9,1,8, 21]))
+// console.log(mode([4,3,1,2,2,7,9,14,2,27,400,9,1,8]))
+
+
+function averagePair(array, targetSum) {
+      array.sort((a, b) => a - b);
+      let left = 0;
+      let right = array.length -1;
+      while (left < right) {
+          const currentSum = array[left] + array[right] / 2;
+          if(currentSum === targetSum) {
+              return true;
+          } else if (currentSum < targetSum) {
+              left++;
+          } else if (currentSum > targetSum) {
+              right--;
+          }
+      }
+      return false;
+  }
+
+  console.log(averagePair([1,3,3,5,6,7,10,12,19],8))
